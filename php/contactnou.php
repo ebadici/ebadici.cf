3d0b2629ed7de684ba00dc95aacf037d2c5f1899
ebadici <43616574+ebadici@users.noreply.github.com>
2018-10-12 19:23:46 +0300

    Add files via upload

---
+++ php/contactnou.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+
+<link rel="stylesheet" href="stylecontactnou.css">
+<div id="php">
+<?php
+
+$servername = "localhost";
+$username = "emil";
+$password = "1234";
+$dbname = "exemplu";
+
+$firstname=$_POST["firstname"];
+$lastname=$_POST["lastname"];
+$phone=$_POST["phone"];
+ 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, phone)
+VALUES  ('$firstname', '$lastname', '$phone')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully!. <br>";
+    echo $sql;
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+<br><a href="agenda.php"><---Înapoi la agendă</a>
+</div>
+</body>
+</html>
